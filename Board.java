76a0a96ccca34d02d8f14cf97ea6ab0556dfa1e3
Ronen Rokhit <r.rokhit11@gmail.com>
2020-05-31 11:47:19 -0400

    wrote skeleton for Board class

--- Board.java
+++ Board.java
@@ -1,4 +1,42 @@
 
 public class Board {
+	
+	/*
+	 * constructor 
+	 * 
+	 * @param int size
+	 */
+	
+	/*
+	 * Determines whether Queen is safe to be placed or not 
+	 * 
+	 * @param int row
+	 * @param int column 
+	 * @return boolean  
+	 */
+	
+	/*
+	 * Place Queen at current index 
+	 * 
+	 * @param int row 
+	 * @param int column 
+	 */
+	
+	/*
+	 * Removes Queen from current index 
+	 * 
+	 * @param int row
+	 * @param int column
+	 */
+	
+	/*
+	 * Prints state of board 
+	 */
+	
+	/*
+	 * Gets size of board 
+	 * 
+	 * @return int 
+	 */
 
 }
