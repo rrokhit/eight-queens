71a8366eede80454b2f439b9320d2ad1b413a52d
Ronen Rokhit <r.rokhit11@gmail.com>
2020-05-31 20:43:34 -0400

    added state(), placeQueen(), removeQueen() and getSize() methods

--- Board.java
+++ Board.java
@@ -1,11 +1,19 @@
+import java.util.Arrays;
 
 public class Board {
 	
+	private int size;
+	private int[][] queens;
+	
 	/*
-	 * constructor 
+	 * Constructor 
 	 * 
 	 * @param int size
 	 */
+	public Board(int size) {
+		this.size = size;
+		this.queens = new int[size][size];
+	}
 	
 	/*
 	 * Determines whether Queen is safe to be placed or not 
@@ -14,29 +22,46 @@ public class Board {
 	 * @param int column 
 	 * @return boolean  
 	 */
+//	public boolean isSafe(int row, int column) {
+//		
+//	}
 	
 	/*
-	 * Place Queen at current index 
+	 * Place Queen at given index 
 	 * 
 	 * @param int row 
 	 * @param int column 
 	 */
+	public void placeQueen(int row, int column) {
+		this.queens[row-1][column-1]++;
+	}
 	
 	/*
-	 * Removes Queen from current index 
+	 * Removes Queen from given index 
 	 * 
 	 * @param int row
 	 * @param int column
 	 */
+	public void removeQueen(int row, int column) {
+		this.queens[row-1][column-1]--;
+	}
 	
 	/*
 	 * Prints state of board 
 	 */
+	public void state() {
+		for(int[] x : this.queens) {
+			System.out.println(Arrays.toString(x));
+		}
+	}
 	
 	/*
 	 * Gets size of board 
 	 * 
 	 * @return int 
 	 */
+	public int getSize() {
+		return this.size;
+	}
 
 }
