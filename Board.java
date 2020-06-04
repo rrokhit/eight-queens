161334a6f423e7de8090c8c5ff2c327f767819cf
Ronen Rokhit <r.rokhit11@gmail.com>
2020-06-04 01:20:45 -0400

    wrote isSafe() method

--- Board.java
+++ Board.java
@@ -1,12 +1,12 @@
 import java.util.Arrays;
 
 public class Board {
-	
+
 	private int size;
 	private int[][] queens;
-	
+
 	/*
-	 * Constructor 
+	 * Constructor
 	 * 
 	 * @param int size
 	 */
@@ -14,51 +14,91 @@ public class Board {
 		this.size = size;
 		this.queens = new int[size][size];
 	}
-	
+
 	/*
-	 * Determines whether Queen is safe to be placed or not 
+	 * Determines whether Queen is safe, at a given index, to be placed or not
 	 * 
 	 * @param int row
-	 * @param int column 
-	 * @return boolean  
-	 */
-//	public boolean isSafe(int row, int column) {
-//		
-//	}
-	
-	/*
-	 * Place Queen at given index 
 	 * 
-	 * @param int row 
-	 * @param int column 
+	 * @param int column
+	 * 
+	 * @return boolean
+	 */
+	public boolean isSafe(int row, int column) {
+		// to manage index
+		int realRow = row - 1;
+		int realCol = column - 1;
+
+		int diagonalRow = realRow;
+		int diagonalCol = realCol;
+
+		// checks the row up till column for any queens
+		for (int x = realCol; x >= 0; x--) {
+			if (this.queens[realRow][x] == 1) {
+				return false;
+			}
+		}
+
+		// checking spots diagonally above and to the left of index
+		while (diagonalRow >= 0 && diagonalCol >= 0) {
+			if (this.queens[diagonalRow][diagonalCol] == 1) {
+				return false;
+			}
+			diagonalRow--;
+			diagonalCol--;
+		}
+
+		// reset diagonal row and column variables for other direction
+		diagonalRow = realRow;
+		diagonalCol = realCol;
+
+		// checking spots diagonally below and to the left of index
+		while (diagonalRow < this.size && diagonalCol >= 0) {
+			if (this.queens[diagonalRow][diagonalCol] == 1) {
+				return false;
+			}
+			diagonalRow++;
+			diagonalCol--;
+		}
+
+		return true;
+	}
+
+	/*
+	 * Place Queen at given index
+	 * 
+	 * @param int row
+	 * 
+	 * @param int column
 	 */
 	public void placeQueen(int row, int column) {
-		this.queens[row-1][column-1]++;
+		this.queens[row - 1][column - 1]++;
 	}
-	
+
 	/*
-	 * Removes Queen from given index 
+	 * Removes Queen from given index
 	 * 
 	 * @param int row
+	 * 
 	 * @param int column
 	 */
 	public void removeQueen(int row, int column) {
-		this.queens[row-1][column-1]--;
+		this.queens[row - 1][column - 1]--;
 	}
-	
+
 	/*
-	 * Prints state of board 
+	 * Prints state of board
 	 */
 	public void state() {
-		for(int[] x : this.queens) {
+		for (int[] x : this.queens) {
 			System.out.println(Arrays.toString(x));
 		}
 	}
-	
+
 	/*
-	 * Gets size of board 
+	 * Gets size of board
 	 * 
-	 * @return int 
+	 * @return int
 	 */
 	public int getSize() {
 		return this.size;
