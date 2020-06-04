1ba1ae77b3139b17385e58611deb08f7daedf679
Ronen Rokhit <r.rokhit11@gmail.com>
2020-06-04 01:43:32 -0400

    wrote private method that handles recursive backtracking algorithm

--- Client.java
+++ Client.java
@@ -2,8 +2,47 @@
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Board b = new Board(8);
+		b.placeQueen(1, 1);
+		b.state();
+		System.out.println(b.isSafe(2, 2));
+		b.removeQueen(2, 2);
+		System.out.println();
+		b.state();
+	}
+	
+	/*
+	 * Client method used to solve board
+	 * 
+	 * @param Board b
+	 */
+	public static void solve(Board b) {
 
+	}
+	
+	/*
+	 * Private method that actually solves problem
+	 * 
+	 * @param Board b
+	 * @param int col
+	 */
+	private static boolean backtrack(Board b, int column) {
+		//success path
+		if(column>b.getSize()) {
+			return true;
+		//exploratory path
+		}else {
+			for(int row = 1 ; row <= b.getSize(); row++) {
+				if(b.isSafe(row, column)) {
+					b.placeQueen(row, column);
+					if(backtrack(b, column +1)) {
+						return true;
+					}
+					b.removeQueen(row, column);
+				}
+			}
+			return false;
+		}
 	}
 
 }
